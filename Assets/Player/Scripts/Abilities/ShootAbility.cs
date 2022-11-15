059276afd6b4fc65f5361536b75074638c6e5524
Ben Rosenmöller <rosenmollerben@gmail.com>
2022-11-16 00:55:48 +0100

    Mostly correct rotations

--- Assets/Player/Scripts/Abilities/ShootAbility.cs
+++ Assets/Player/Scripts/Abilities/ShootAbility.cs
@@ -9,9 +9,8 @@ public class ShootAbility : PlayerAbility
     [SerializeField] private GameObject bullet;
 
     private Camera mainCamera;
-
+    private Vector2 mouseScreenPosition;
     private Vector2 shootDirection;
-    private Quaternion targetRotation;
 
     protected override void SetAbilityType() => abilityType = PlayerAbilitys.Shoot;
 
@@ -21,34 +20,39 @@ public class ShootAbility : PlayerAbility
 
         abilityManager.controls.Default.Shoot.performed += _ => Shoot();
         abilityManager.controls.Default.Aiming.performed += ctx => SetShootDirection(ctx.ReadValue<Vector2>());
+        abilityManager.controls.Default.Movement.started += _ => UpdateShootDirection();
     }
 
     private void SetShootDirection(Vector2 vector2Input)
     {
-        Debug.Log(abilityManager.usingGamepad);
         if (abilityManager.usingGamepad)
         {
-            Debug.Log("dsf");
-            shootDirection = vector2Input;
+            if (vector2Input != Vector2.zero) shootDirection = vector2Input;
         }
         else
         {
-            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(vector2Input);
-            Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
-
-            shootDirection = direction;
+            mouseScreenPosition = vector2Input;
+            Vector2 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+            shootDirection = (mouseWorldPosition - (Vector2)transform.position).normalized;
         }
 
-        float angle = Vector2.SignedAngle(transform.position, shootDirection);
-        targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        LookTowardsShootDirection();
     }
 
-    private void Update()
+    private void UpdateShootDirection()
     {
-        if (targetRotation != transform.rotation)
-        {
-            Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
-        }
+        if (abilityManager.usingGamepad) { return; }
+
+        Vector2 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+        shootDirection = (mouseWorldPosition - (Vector2)transform.position).normalized;
+
+        LookTowardsShootDirection();
+    }
+
+    private void LookTowardsShootDirection()
+    {
+        float angle = Vector2.SignedAngle(Vector2.right, shootDirection);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
     }
 
     private void Shoot()
