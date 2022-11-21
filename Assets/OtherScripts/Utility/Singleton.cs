d643042cb2e5478d67dd7570e977ef5e35f774dc
Ben Rosenmöller <rosenmollerben@gmail.com>
2022-11-21 13:56:34 +0100

    Backup

---
+++ Assets/OtherScripts/Utility/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                DontDestroyOnLoad(instance.gameObject);
+            }
+
+            return instance;
+            
+        }
+    }
+
+    //protected virtual void Awake()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = GetComponent<T>();
+    //        DontDestroyOnLoad(instance.gameObject);
+    //    }
+    //    else if (instance.GetInstanceID() != GetInstanceID())
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
